db8e806ea08209414dd4d9bb09c947907944d1ea
Philip Hodder <philip.hodder@gmail.com>
2015-08-23 00:20:10 +1200

    Added base file, no API specified yet

---
+++ src/gameLogic/GameController.java
@@ -0,0 +1,13 @@
+/**
+ * GameController is an interface between a game interface, and 
+ * the game logic. 
+ *
+ * <em>This is currently place holder. Yet to be fully defined</em>
+ *
+ * @author Philip Hodder
+ * @since 2015-08-23
+ */
+
+public interface GameController {
+    
+}
