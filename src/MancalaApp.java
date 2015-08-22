6374cc71abb08e497b763a9e46883ede1b69d7b4
Philip Hodder <philip.hodder@gmail.com>
2015-08-23 01:01:57 +1200

    Created main method, basic hello world as of now.

---
+++ src/MancalaApp.java
@@ -0,0 +1,21 @@
+/**
+ * MancalaApp is the entry point of the application. 
+ *
+ * @author
+ * @since 2015-08-23
+ */
+
+public class MancalaApp {
+
+    /**
+     * Currently, prints out "Hello, World!"
+     * Later, will instantiate necessary classes. May handle
+     * some command line arguments too (unsure as of now). 
+     *
+     * @param args: Currently unused.
+     */
+    public static void main(String [] args) {
+	System.out.println("Hello, World!");
+    }
+    
+}
