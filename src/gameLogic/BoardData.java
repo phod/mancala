335fb539677ba9ca6266a284d4a1b5db3a712002
Ross Hanson <roscohanson@gmail.com>
2016-02-23 16:42:20 +1300

    BoardData method getPieces now throws ArrayIndexOutOfBoundsException instead of unnecessary custom exceptions.

--- src/gameLogic/BoardData.java
+++ src/gameLogic/BoardData.java
@@ -9,54 +9,66 @@ package gameLogic;
 
 public class BoardData {
 
-    private final int SIDE_ONE = 0;
-    private final int SIDE_TWO = 1;
-    
-    private int[][] holes;
-    private int[] mancalas;
-    
-    /**
-     * Creates a board with 2 sides, 6 holes on each side, and 2
-     * mancala holes. Each hole (excluding mancalas) is filled with
-     * 4 pieces. 
-     */
-    public BoardData() {
-	this.holes = new int[2][6];
-	this.mancalas = new int[2];
-	//Add 4 pieces to each hole.
-	for (int i = 0; i < this.holes.length; i++) {
-	    this.addPieces(SIDE_ONE, i, 4);
-	    this.addPieces(SIDE_TWO, i, 4);
+	private final int SIDE_ONE = 0;
+	private final int SIDE_TWO = 1;
+
+	private int[][] holes;
+	private int[] mancalas;
+
+	/**
+	 * Creates a board with 2 sides, 6 holes on each side, and 2 mancala holes.
+	 * Each hole (excluding mancalas) is filled with 4 pieces.
+	 */
+	public BoardData() {
+		this.holes = new int[2][6];
+		this.mancalas = new int[2];
+		// Add 4 pieces to each hole.
+		for (int i = 0; i < this.holes.length; i++) {
+			this.addPieces(SIDE_ONE, i, 4);
+			this.addPieces(SIDE_TWO, i, 4);
+		}
 	}
-    }
 
-    /**
-     * Adds the specified number of pieces to the specified hole.
-     * This method does <em>not</em> add to mancala holes.
-     *
-     * @param side The side of the board to add the pieces to.
-     * @param pos Which hole on the board to add the piece to.
-     * @param numPieces How many pieces to add to the hole.
-     * @throws InvalidSideException TODO
-     * @throws InvalidPositionException TODO
-     * @throws NegativePiecesException TODO
-     */
-    public void addPieces(int side, int pos, int numPieces) {
-	this.holes[side][pos] += numPieces;
-    }
+	/**
+	 * Adds the specified number of pieces to the specified hole. This method
+	 * does <em>not</em> add to mancala holes.
+	 *
+	 * @param side
+	 *            The side of the board to add the pieces to.
+	 * @param pos
+	 *            Which hole on the board to add the piece to.
+	 * @param numPieces
+	 *            How many pieces to add to the hole.
+	 * @throws InvalidSideException
+	 *             TODO
+	 * @throws InvalidPositionException
+	 *             TODO
+	 * @throws NegativePiecesException
+	 *             TODO
+	 */
+	public void addPieces(int side, int pos, int numPieces) {
+		this.holes[side][pos] += numPieces;
+	}
 
-    /**
-     * Returns the number of pieces in the specified hole.
-     * This method does <em>not</em> retrieve from mancala holes.
-     *
-     * @param side The side of the board to see the number of pieces.
-     * @param pos Which hole on the board to see the number of pieces.
-     * @return The number of pieces in the request position.
-     * @throws InvalidSideException TODO
-     * @throws InvalidPositionExcpetion TODO
-     */
-    public int getPieces(int side, int pos) {
-	return this.holes[side][pos];
-    }
-    
+	/**
+	 * Returns the number of pieces in the specified hole. This method does
+	 * <em>not</em> retrieve from mancala holes.
+	 *
+	 * @param side
+	 *            The side of the board to see the number of pieces.
+	 * @param pos
+	 *            Which hole on the board to see the number of pieces.
+	 * @return The number of pieces in the request position.
+	 * @throws ArrayIndexOutOfBoundsException
+	 */
+	public int getPieces(int side, int pos) {
+		if (side < 0 || side > 1) {
+			throw new ArrayIndexOutOfBoundsException("invalid side");
+		}
+		if (pos < 0 || pos > 5) {
+			throw new ArrayIndexOutOfBoundsException("invalid posistion");
+		}
+		return this.holes[side][pos];
+	}
+
 }
