6bc6a6d2543bcf29d554f2224921eada43e81b6c
Ross Hanson <roscohanson@gmail.com>
2016-04-08 16:19:24 +1200

    First test for boardData's isHoleEmpty

--- tests/BoardDataTest.java
+++ tests/BoardDataTest.java
@@ -3,9 +3,12 @@ import org.junit.Test;
 import gameLogic.BoardData;
 
 public class BoardDataTest {
+	
     
     @Test
-    public void testIsEmptyBoard() {
-	assertEquals(0, 0);
+    BoardData test = new BoardData(1, 1);
+    test.sides[0][0]=0;
+    public void testIsEmptyBoard() {   	
+    	assertEquals("Empty hole must be true",true, test.isHoleEmpty(0,0));
     }
 }
